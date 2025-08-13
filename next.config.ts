3b4a3c3d10705185f933289984bc4b5c9be53dc8
Ian DiBruno <idibruno@gmail.com>
2025-08-12 21:47:46 -0700

    Add image config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,13 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'afnrmkdjxpzjoxhnfdxv.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+
     ],
   },
 };
